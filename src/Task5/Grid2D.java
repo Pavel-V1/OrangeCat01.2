cf79fd4404e677841664caff3e02fae7748432a4
Pavel-V1 <pavelvoronov009@gmail.com>
2025-12-11 11:00:47 +0300

    gui

---
+++ src/Task5/Grid2D.java
@@ -0,0 +1,8 @@
+package Task5;
+
+public class Grid2D {
+    Grid grid = new Grid();
+    Integer[][] grid2D = new Integer[grid.getSizeByYValue()][grid.getSizeByYValue()];
+
+    //
+}
--- src/Task5/Main.java
+++ src/Task5/Main.java
@@ -1,7 +1,9 @@
 package Task5;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, cat!");
+//        System.out.println("Hello, cat!");
     }
 }
